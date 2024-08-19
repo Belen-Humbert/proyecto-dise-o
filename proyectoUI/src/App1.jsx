78021e5ed52e7cc449b204766595e890fe4ab7e2
AlejoMontuori <Alejomontuori12@gmail.com>
2024-08-19 17:53:42 -0300

    commit de prueba

---
+++ proyectoUI/src/App1.jsx
@@ -0,0 +1,16 @@
+ export default function App1(){
+
+    return(
+        <>
+        
+        <footer>
+        <p>Hola este el pieeeeeeeeeeee</p>
+        </footer>
+        
+        </>
+
+
+    )
+
+
+}
