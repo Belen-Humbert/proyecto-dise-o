07a3e532b4a46214435da3493cac6d98b22d3fa8
AlejoMontuori <Alejomontuori12@gmail.com>
2024-09-03 19:38:44 -0300

    footer añadido a app

--- proyectoUI/src/App.jsx
+++ proyectoUI/src/App.jsx
@@ -1,5 +1,9 @@
 import "./Styles/App.css";
-import Pie from "./components/footer.jsx";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 
 function App() {
   return (
@@ -145,8 +149,53 @@ function App() {
       </div>
       {/* /.banner */}
       {/* Footer */}
-      <Pie />
-      {/* */}
+      <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <h2>AuditaWeb Solutions</h2>
+        </div>
+        <div className="footer-center">
+          <ul className="footer-menu">
+            <li>About</li>
+            <li>Inicio</li>
+            <li>
+              <details>
+              <summary>Contacto</summary>
+              <li>261360238</li>
+              <li>233231312</li>
+              <li>alwwksmxskxS</li>
+              </details>
+            </li>
+            <li>Auditorías</li>
+          </ul>
+          <p>© 2024 IWDS. All rights reserved.</p>
+        </div>
+        <div className="footer-right">
+          <ul className="footer-socials">
+            <li>
+              <a href="https://www.facebook.com/sebastian.montuori.1">
+                <FacebookIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/">
+                <XIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/">
+                <InstagramIcon />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/bel%C3%A9n-humbert/">
+                <LinkedInIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>      {/* */}
     </>
   );
 }
