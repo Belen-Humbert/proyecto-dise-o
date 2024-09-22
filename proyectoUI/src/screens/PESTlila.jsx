b31f844907d408df342819512da815551343773f
Belen Humbert <belenhumbert69@gmail.com>
2024-09-22 19:55:51 -0300

    Modificación estética de la página de Lila: agregado de imágenes, agregado de cards, etc

---
+++ proyectoUI/src/screens/PESTlila.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PESTlila = () => {
+  return (
+    <div>
+      <h1>Análisis PEST</h1>
+      <p>Esta es la página del Análisis PEST.</p>
+    </div>
+  );
+};
+
+export default PESTlila;  // Exportar el componente como default
---
+++ proyectoUI/src/screens/PORTERlila.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PORTERlila = () => {
+  return (
+    <div>
+      <h1>Análisis 5 Fuerzas de Porter</h1>
+      <p>Esta es la página del Análisis de las 5 Fuerzas de Porter.</p>
+    </div>
+  );
+};
+
+export default PORTERlila;  // Exportar el componente como default
