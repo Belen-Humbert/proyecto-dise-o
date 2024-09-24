c115ddde824ec16d851b054c299c35394e3a63b0
Belen Humbert <belenhumbert69@gmail.com>
2024-09-23 21:37:07 -0300

    Agregados estilos e imágenes para los análisis PEST y PORTER, y actualizadas las pantallas

--- proyectoUI/src/screens/PESTlila.jsx
+++ proyectoUI/src/screens/PESTlila.jsx
@@ -1,12 +1,195 @@
 import React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import styles from '../Styles/PESTlila.module.css';
+import PESTImage from '../assets/imagenes/PESTlila.png';
 
 const PESTlila = () => {
   return (
-    <div>
-      <h1>Análisis PEST</h1>
-      <p>Esta es la página del Análisis PEST.</p>
+    <><div className={styles.pestLilaContainer}>
+      <Typography
+        variant="h2"
+        sx={{
+          fontFamily: 'Poppins, sans-serif',
+          fontWeight: 700,
+          color: '#936A9F',
+          fontSize: '3rem',
+          marginBottom: '40px',
+          textAlign: 'center'
+        }}
+      >
+        Análisis PEST
+      </Typography>
+
+      <div className={styles.accordionWrapper}>
+        {/* Estructura Económica */}
+        <Accordion
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="h5" sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}>
+              Estructura Económica
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails sx={{ backgroundColor: '#f9f9f9' }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7
+              }}
+            >
+              <strong>Tasa de Crecimiento Económico:</strong> En Argentina, la desaceleración económica es un factor crítico. Actualmente, la falta de inversión en muchos sectores debido a la incertidumbre económica afecta la demanda de servicios de software. Lila, al operar en este entorno, enfrenta desafíos para captar nuevos clientes tanto locales como internacionales, ya que el poder adquisitivo ha disminuido y los negocios son más cautelosos con sus inversiones en tecnología.
+              <br /><br />
+              <strong>Inflación y Tasas de Interés:</strong> La inflación en Argentina es uno de los factores económicos más relevantes, ya que los costos operativos, incluidos los salarios y los insumos tecnológicos, aumentan de manera continua. Esto afecta los márgenes de ganancia de la empresa. Además, las elevadas tasas de interés dificultan el acceso a financiamiento, lo que limita las posibilidades de inversión en tecnología y expansión.
+              <br /><br />
+              <strong>Disponibilidad de Capital y Financiación:</strong> Existen programas gubernamentales y privados que apoyan el financiamiento de startups y empresas tecnológicas. Sin embargo, Lila ha optado por autofinanciarse hasta ahora, lo que puede limitar su crecimiento. La opción de acceder a capital de riesgo podría ser una estrategia futura para impulsar la expansión y la innovación.
+              <br /><br />
+              <strong>Tipo de Cambio y Competitividad:</strong> El tipo de cambio en Argentina varía considerablemente, lo que puede afectar la competitividad de los productos y servicios de Lila en el exterior. Si bien el mercado local puede volverse más costoso, las exportaciones de software se vuelven más atractivas debido a la depreciación de la moneda, brindando oportunidades para internacionalizar sus servicios.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Estructura Tecnológica */}
+        <Accordion
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="h5" sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}>
+              Estructura Tecnológica
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails sx={{ backgroundColor: '#f9f9f9' }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7
+              }}
+            >
+              <strong>Innovación y Desarrollo Tecnológico:</strong> El uso de tecnologías emergentes, como la Inteligencia Artificial (IA), es una de las tendencias clave que está transformando la industria del software. Lila ha comenzado a integrar IA en su flujo de trabajo para automatizar tareas y mejorar la eficiencia en el desarrollo de productos, lo que representa una ventaja competitiva en el mercado.
+              <br /><br />
+              <strong>Adopción de Nuevas Tecnologías:</strong> Lila ha demostrado estar abierta a incorporar nuevas tecnologías que mejoren la calidad de sus productos y servicios. Herramientas como Google Cloud, Figma, y Notion son fundamentales para la operatividad diaria, y su flexibilidad para adoptar nuevas tecnologías les permite mantenerse competitivos en un mercado que avanza rápidamente. Sin embargo, la empresa debe continuar evaluando qué nuevas herramientas pueden mejorar aún más su oferta y optimizar sus procesos.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Estructura Política */}
+        <Accordion
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="h5" sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}>
+              Estructura Política
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails sx={{ backgroundColor: '#f9f9f9' }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7
+              }}
+            >
+              <strong>Estabilidad Política:</strong> Argentina enfrenta una considerable inestabilidad política que afecta tanto a la seguridad jurídica como a la estabilidad de las políticas económicas y laborales. Esto genera incertidumbre para empresas como Lila, que deben adaptarse rápidamente a cambios en la normativa fiscal o en las regulaciones que impactan su capacidad para operar.
+              <br /><br />
+              <strong>Regulaciones y Políticas Gubernamentales:</strong> A pesar de la inestabilidad, hay iniciativas locales que favorecen la innovación tecnológica. Programas como Mendoza Activa y Mendoza Innova incentivan la contratación formal y el desarrollo de empresas tecnológicas, lo que puede ayudar a Lila a captar recursos y expandirse dentro del mercado provincial con el apoyo de incentivos fiscales y financieros.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Estructura Social */}
+        <Accordion
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography variant="h5" sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}>
+              Estructura Social
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails sx={{ backgroundColor: '#f9f9f9' }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7
+              }}
+            >
+              <strong>Demografía y Fuerza Laboral:</strong> Lila tiene una limitación en su capacidad para contratar talento, ya que prefiere trabajadores que estén ubicados cerca de sus oficinas para mantener un equilibrio entre el trabajo remoto y presencial. Esto puede ser un desafío en términos de reclutamiento, dado que restringe el acceso a una mayor diversidad de talento, especialmente en roles técnicos especializados que podrían encontrarse en otras regiones o países.
+              <br /><br />
+              <strong>Tendencias Sociales y Culturales:</strong> La adopción de herramientas low-code y no-code está en aumento, lo que podría representar tanto una oportunidad como una amenaza para Lila. Por un lado, estas tecnologías permiten a los clientes crear soluciones más rápidamente y a menor costo; por otro lado, podrían reducir la demanda de desarrollo de software a medida, que es uno de los servicios principales de Lila. La empresa deberá adaptarse a esta tendencia, ofreciendo servicios de mayor valor agregado que justifiquen la personalización del producto.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </div>
+    <div className={styles.pestLilaImageContainer}>
+        <img src={PESTImage} alt="PEST Lila" className={styles.pestLilaImage} />
+      </div></>
   );
 };
 
-export default PESTlila;  // Exportar el componente como default
+export default PESTlila;
--- proyectoUI/src/screens/PORTERlila.jsx
+++ proyectoUI/src/screens/PORTERlila.jsx
@@ -1,12 +1,358 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import styles from '../Styles/PORTERlila.module.css';
+import PORTERlilaImage from '../assets/imagenes/PORTERlila.png';
 
 const PORTERlila = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
   return (
-    <div>
-      <h1>Análisis 5 Fuerzas de Porter</h1>
-      <p>Esta es la página del Análisis de las 5 Fuerzas de Porter.</p>
+    <div className={styles.porterLilaContainer}>
+      <Typography
+        variant="h2"
+        sx={{
+          fontFamily: 'Poppins, sans-serif !important',
+          fontWeight: 700,
+          color: '#c1a6cf',  // Color del título
+          fontSize: '3rem',
+          marginBottom: '40px',
+          textAlign: 'center',
+        }}
+      >
+        Análisis 5 Fuerzas de Porter
+      </Typography>
+        
+      {/* Párrafo centrado debajo del título */}
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: 'Poppins, sans-serif !important',
+          fontSize: '1.2rem',
+          color: '#666',
+          maxWidth: '700px',
+          margin: '0 auto 40px auto', // Centrado y espacio inferior
+          textAlign: 'center',
+          lineHeight: 1.7,
+        }}
+      >
+        El análisis de las 5 Fuerzas de Porter examina la competitividad de Lila Software Studio dentro de su industria,
+        evaluando el poder de negociación de los proveedores y clientes, la amenaza de nuevos competidores y productos 
+        sustitutos, y la rivalidad entre competidores.
+      </Typography>
+
+      <div className={styles.accordionWrapper}>
+        {/* Acordeón: Poder de Negociación de los Proveedores */}
+        <Accordion
+          expanded={expanded === 'panel1'}
+          onChange={handleChange('panel1')}
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: expanded === 'panel1' ? '#ffffff' : '#c1a6cf',  // Cambiar el fondo del acordeón
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: expanded === 'panel1' ? '#c1a6cf' : 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',  // Cambiar el fondo del acordeón cerrado
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}
+            >
+              Poder de Negociación de los Proveedores
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            sx={{
+              backgroundColor: '#ffffff', // Fondo blanco cuando se abre
+              color: '#666',
+              maxWidth: '700px',
+              margin: '0 auto',
+              padding: '20px',
+              textAlign: 'center',
+              lineHeight: 1.7,
+              fontFamily: 'Poppins, sans-serif !important',
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif !important',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7,
+              }}
+            >
+              Lila depende de una serie de proveedores de herramientas tecnológicas esenciales para su operación diaria, 
+              como Google Cloud (para la infraestructura en la nube), Notion (para la gestión de proyectos), Figma 
+              (para el diseño de interfaces), y Slack (para la comunicación interna). Estas plataformas son fundamentales 
+              para mantener la eficiencia y productividad de sus equipos, pero también representan un riesgo. Cambiar de 
+              proveedor no es sencillo, ya que las herramientas mencionadas están profundamente integradas en los flujos 
+              de trabajo de la empresa. La dependencia de estas plataformas crea una barrera para cambiar de proveedor, lo 
+              que puede hacer a Lila vulnerable ante aumentos de costos o cambios en las condiciones de uso. Además, el 
+              hecho de que estas herramientas sean estándares en la industria tecnológica reduce la posibilidad de negociar 
+              precios más competitivos.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Acordeón: Poder de Negociación de los Clientes */}
+        <Accordion
+          expanded={expanded === 'panel2'}
+          onChange={handleChange('panel2')}
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: expanded === 'panel2' ? '#ffffff' : '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: expanded === 'panel2' ? '#c1a6cf' : 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}
+            >
+              Poder de Negociación de los Clientes
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            sx={{
+              backgroundColor: '#ffffff', // Fondo blanco cuando se abre
+              color: '#666',
+              maxWidth: '700px',
+              margin: '0 auto',
+              padding: '20px',
+              textAlign: 'center',
+              lineHeight: 1.7,
+              fontFamily: 'Poppins, sans-serif !important',
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif !important',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7,
+              }}
+            >
+              Lila tiene la ventaja de trabajar con clientes que valoran la calidad y la personalización de sus productos, 
+              lo que les otorga cierta flexibilidad en términos de fijación de precios. Sin embargo, los clientes también 
+              tienen opciones para cambiar de proveedor si no están satisfechos con los costos o el servicio. La personalización 
+              y el alto nivel de involucramiento del cliente durante el proceso de desarrollo ayudan a fidelizar a los clientes 
+              actuales, pero Lila debe ser consciente de que en un mercado con más competencia y nuevas soluciones tecnológicas, 
+              el poder de negociación de los clientes podría aumentar.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Acordeón: Amenaza de Nuevos Competidores */}
+        <Accordion
+          expanded={expanded === 'panel3'}
+          onChange={handleChange('panel3')}
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: expanded === 'panel3' ? '#ffffff' : '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: expanded === 'panel3' ? '#c1a6cf' : 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}
+            >
+              Amenaza de Nuevos Competidores
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            sx={{
+              backgroundColor: '#ffffff', // Fondo blanco cuando se abre
+              color: '#666',
+              maxWidth: '700px',
+              margin: '0 auto',
+              padding: '20px',
+              textAlign: 'center',
+              lineHeight: 1.7,
+              fontFamily: 'Poppins, sans-serif !important',
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif !important',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7,
+              }}
+            >
+              Aunque Lila está bien posicionada en Mendoza, la barrera de entrada en la industria del software es relativamente 
+              baja en términos tecnológicos. Nuevas empresas pueden ingresar al mercado ofreciendo precios más bajos o servicios 
+              especializados, lo que podría aumentar la competencia en el futuro. Sin embargo, la reputación de Lila y su enfoque 
+              en la personalización del software les otorgan una ventaja competitiva que puede ser difícil de replicar por nuevos 
+              actores, especialmente si Lila sigue desarrollando productos propios innovadores.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Acordeón: Amenaza de Productos Sustitutos */}
+        <Accordion
+          expanded={expanded === 'panel4'}
+          onChange={handleChange('panel4')}
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: expanded === 'panel4' ? '#ffffff' : '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: expanded === 'panel4' ? '#c1a6cf' : 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}
+            >
+              Amenaza de Productos Sustitutos
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            sx={{
+              backgroundColor: '#ffffff', // Fondo blanco cuando se abre
+              color: '#666',
+              maxWidth: '700px',
+              margin: '0 auto',
+              padding: '20px',
+              textAlign: 'center',
+              lineHeight: 1.7,
+              fontFamily: 'Poppins, sans-serif !important',
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif !important',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7,
+              }}
+            >
+              Las soluciones low-code y no-code representan una amenaza significativa para las empresas que desarrollan software 
+              a medida. Estas herramientas permiten a las empresas crear aplicaciones básicas sin necesidad de contratar desarrolladores. 
+              Aunque estas soluciones no están tan personalizadas como los productos que Lila ofrece, representan una alternativa atractiva 
+              para empresas con presupuestos más ajustados o menos complejidad en sus necesidades tecnológicas. Lila debe responder a esta 
+              amenaza ofreciendo un nivel de personalización y soporte que no puede ser igualado por soluciones automáticas, destacando 
+              el valor de sus servicios a medida.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        {/* Acordeón: Rivalidad entre Competidores */}
+        <Accordion
+          expanded={expanded === 'panel5'}
+          onChange={handleChange('panel5')}
+          sx={{
+            marginBottom: '20px',
+            borderRadius: '8px',
+            backgroundColor: expanded === 'panel5' ? '#ffffff' : '#c1a6cf',
+          }}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: expanded === 'panel5' ? '#c1a6cf' : 'white' }} />}
+            sx={{
+              backgroundColor: '#c1a6cf',
+              color: 'white',
+              fontFamily: 'Poppins, sans-serif',
+              fontWeight: 700,
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ fontFamily: 'Poppins, sans-serif', fontWeight: 700 }}
+            >
+              Rivalidad entre Competidores
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails
+            sx={{
+              backgroundColor: '#ffffff', // Fondo blanco cuando se abre
+              color: '#666',
+              maxWidth: '700px',
+              margin: '0 auto',
+              padding: '20px',
+              textAlign: 'center',
+              lineHeight: 1.7,
+              fontFamily: 'Poppins, sans-serif !important',
+            }}
+          >
+            <Typography
+              variant="body1"
+              sx={{
+                fontFamily: 'Poppins, sans-serif !important',
+                fontSize: '1.2rem',
+                color: '#666',
+                lineHeight: 1.7,
+              }}
+            >
+              En Mendoza, Lila se encuentra en un entorno con pocos competidores directos que ofrezcan el mismo nivel de personalización. 
+              Sin embargo, la competencia a nivel internacional es mucho más intensa. La empresa se enfrenta a competidores globales 
+              que pueden ofrecer servicios a menor costo, especialmente debido a la diferencia en el tipo de cambio. La capacidad de Lila 
+              para mantener su ventaja competitiva radica en su enfoque en la calidad, la cercanía con el cliente y su capacidad para 
+              adaptarse rápidamente a las nuevas demandas del mercado tecnológico. Sin embargo, para mantenerse competitiva, Lila debe 
+              continuar innovando y asegurarse de que sus productos y servicios ofrezcan valor agregado que sus competidores no puedan 
+              replicar fácilmente.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+
+      {/* Imagen */}
+      <div className={styles.imageContainer}>
+        <img
+          src={PORTERlilaImage}
+          alt="Análisis 5 Fuerzas de Porter"
+          className={styles.porterLilaImage}
+        />
+      </div>
     </div>
   );
 };
 
-export default PORTERlila;  // Exportar el componente como default
+export default PORTERlila;
