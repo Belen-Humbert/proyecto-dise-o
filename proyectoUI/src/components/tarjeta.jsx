e4c7a828d8ddb8371d4ba94a9cc399d610cc7479
AlejoMontuori <Alejomontuori12@gmail.com>
2024-10-07 21:49:36 -0300

    mejoras en las cards

--- proyectoUI/src/components/tarjeta.jsx
+++ proyectoUI/src/components/tarjeta.jsx
@@ -1,24 +1,20 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
-export default function Tarjeta({empresa}) {
+export default function Tarjeta({ empresa }) {
   return (
     <Card sx={{ maxWidth: 600 }}>
       <CardActionArea>
-        <CardMedia
-         component="img"
-          image={empresa.imagen}
-          alt={empresa.name}
-        />
-        <CardContent>
-          <Typography gutterBottom  variant="h5" component="div">
+        <CardMedia component="img" image={empresa.imagen} alt={empresa.name} />
+        <CardContent sx={{ backgroundColor: "#141517"}}>
+          <Typography gutterBottom variant="h5" component="div" sx={{color: '#aba7a6'}}>
             {empresa.name}
           </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          <Typography variant="body2" sx={{color: '#aba7a6'}} >
             {empresa.descript}
           </Typography>
         </CardContent>
